14f804379fd5e9e2f0e38e9543b53dc0d9a95319
MauroFGarcia87 <maurofgarcia87@gmail.com>
2024-10-17 00:55:32 -0300

    Agregue la parte para mostrar los datos en el html

--- js/app.js
+++ js/app.js
@@ -1,34 +1,60 @@
 const productos = [
   {
-    nombre: "Caña",
-    cantidad: 10
-  },
+  nombre: "leña",
+  cantidad: 100,
+}
 ];
 
 let num = 0;
 
-let productoNombre = document.getElementById('nombreProducto').value;
-const formProducto = document.getElementById('formProducto');
-const sectionProductos = document.getElementById('mostrarProducto');
+let productoNombre = document.getElementById("nombreProducto").value;
+const formProducto = document.getElementById("formProducto");
+const sectionProductos = document.getElementById("mostrarProducto");
 
-formProducto.addEventListener('submit', agregar);
+formProducto.addEventListener("submit", agregar);
 
-
-
-function agregar(e){
-    e.preventDefault();
-    let productoNombre = document.getElementById('nombreProducto').value;
-    let productoCantidad = document.getElementById('cantidadProducto').value;
-    let nuevoProducto = {
-        nombre: productoNombre,
-        cantidad: productoCantidad
-    }
-
-    productos.push(nuevoProducto);
-    console.log(productos);
+function borrarLista(){
+  let containerMostrarProdcutos = document.getElementById('containerMostrarProdcutos');
+  let contenedor = document.getElementById('mostrarProductos');
+  contenedor.remove()
+  let divMostrarProductos = document.createElement("div");
+  divMostrarProductos.innerHTML = `<div id="mostrarProductos">`
+  containerMostrarProdcutos.append(divMostrarProductos);
 }
 
+function agregar(e) {
+  e.preventDefault();
+  let productoNombre = document.getElementById("nombreProducto").value;
+  let productoCantidad = document.getElementById("cantidadProducto").value;
+  let nuevoProducto = {
+    nombre: productoNombre,
+    cantidad: productoCantidad,
+  };
+
+  productos.push(nuevoProducto);
+  console.log(productos);
+ 
+  borrarLista();
+  mostrarProductos(productos);
+  
+}
+
+function mostrarProductos(productos) {
+  
+  let contenedorProductos = document.getElementById("mostrarProductos");
+  console.log(contenedorProductos);
+  for (const producto of productos) {
+    contenedor = document.createElement("div")
+    contenedor.innerHTML = `<div  class="d-flex ">
+                                        <h5 class="mx-5">Producto: </h5>
+                                        <p>  ${producto.nombre}</p>
+                                        <h5 class="mx-5">Cantidad: </h5>
+                                        <p>  ${producto.cantidad}</p>`;
+    contenedorProductos.appendChild(contenedor);
+  }
+
+}
+
+mostrarProductos(productos);
+
 console.log(productos);
-
-
-
