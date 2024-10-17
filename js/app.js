a8b7e4a22cebc9f56ec9ed25c489ef538fe0e9e9
MauroFGarcia87 <maurofgarcia87@gmail.com>
2024-10-17 12:10:25 -0300

    Agregamos estilos y la funcion contar stock

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 const productos = [
   {
-  nombre: "leña",
-  cantidad: 100,
-}
+    nombre: "Bolsa de dormir",
+    cantidad: 50,    
+  },
+  {
+    nombre: "Linterna",
+    cantidad: 20, 
+  },
 ];
 
 let num = 0;
@@ -13,13 +17,39 @@ const sectionProductos = document.getElementById("mostrarProducto");
 
 formProducto.addEventListener("submit", agregar);
 
-function borrarLista(){
-  let containerMostrarProdcutos = document.getElementById('containerMostrarProdcutos');
-  let contenedor = document.getElementById('mostrarProductos');
-  contenedor.remove()
+function borrarStock() {
+
+  const containerBorrarStock = document.getElementById('containerStock');
+
+ 
+  let contenedor = document.getElementById("canditadStock");
+  contenedor.remove();
   let divMostrarProductos = document.createElement("div");
-  divMostrarProductos.innerHTML = `<div id="mostrarProductos">`
+  divMostrarProductos.innerHTML = `<div id="canditadStock">`;
+  containerBorrarStock.append(divMostrarProductos);
+}
+
+function borrarLista() {
+  let containerMostrarProdcutos = document.getElementById(
+    "containerMostrarProdcutos"
+  );
+  let contenedor = document.getElementById("mostrarProductos");
+  contenedor.remove();
+  let divMostrarProductos = document.createElement("div");
+  divMostrarProductos.innerHTML = `<div id="mostrarProductos">`;
   containerMostrarProdcutos.append(divMostrarProductos);
+}
+
+function contarStock(productos){
+  let cantidadStock = 0;
+  for (const cant of productos){
+    cantidadStock = cantidadStock + parseInt(cant.cantidad) ;
+  }
+  const contenedorCantidad = document.getElementById('canditadStock');
+  const contenedor = document.createElement('div');
+  contenedor.innerHTML = `<h4> Total:  ${cantidadStock}</h4>`;
+  contenedorCantidad.append(contenedor);
+  console.log(cantidadStock);
 }
 
 function agregar(e) {
@@ -33,28 +63,32 @@ function agregar(e) {
 
   productos.push(nuevoProducto);
   console.log(productos);
- 
+
   borrarLista();
   mostrarProductos(productos);
+  borrarStock();
+  contarStock(productos);
   
 }
 
 function mostrarProductos(productos) {
-  
   let contenedorProductos = document.getElementById("mostrarProductos");
   console.log(contenedorProductos);
   for (const producto of productos) {
-    contenedor = document.createElement("div")
+    const contenedor = document.createElement("div");
+    
     contenedor.innerHTML = `<div  class="d-flex ">
                                         <h5 class="mx-5">Producto: </h5>
                                         <p>  ${producto.nombre}</p>
                                         <h5 class="mx-5">Cantidad: </h5>
-                                        <p>  ${producto.cantidad}</p>`;
+                                        <p>  ${producto.cantidad}</p>
+                                        `;
+                                       
     contenedorProductos.appendChild(contenedor);
   }
-
 }
 
 mostrarProductos(productos);
+contarStock(productos);
 
 console.log(productos);
